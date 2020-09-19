ae27798b89309f1228a8455e418efdde3b60a529
Gaurang Pateriya <45228795+gaurangpateriya@users.noreply.github.com>
2020-09-19 20:16:58 +0530

    Add files via upload

---
+++ precache-manifest.d8d98de894d4894ee78e4cbe98e61d19.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61a7e92504b95213955d720d7d16127",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0e081d285184225c845",
+    "url": "/static/css/2.96778cc5.chunk.css"
+  },
+  {
+    "revision": "7c91ec45f876a0ff1070",
+    "url": "/static/css/main.4521fba0.chunk.css"
+  },
+  {
+    "revision": "f0e081d285184225c845",
+    "url": "/static/js/2.dd736457.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.dd736457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c91ec45f876a0ff1070",
+    "url": "/static/js/main.a9ce47f4.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2597b9deaa94d141046a7dc095fb32f7",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.08.11 PM (1).2597b9de.jpeg"
+  },
+  {
+    "revision": "132a684e82f5c6535868e8c9dce96559",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.08.11 PM.132a684e.jpeg"
+  },
+  {
+    "revision": "6fa6f2a8519ddb48dd5acc5fe03f6e9d",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.08.12 PM.6fa6f2a8.jpeg"
+  },
+  {
+    "revision": "30a5b660395161845fce367ff566fd75",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.02 PM.30a5b660.jpeg"
+  },
+  {
+    "revision": "1bae686ba14ae7440fb20a085cd74389",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.03 PM (1).1bae686b.jpeg"
+  },
+  {
+    "revision": "69a7842edcd87410afc803ff4b8f884b",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.03 PM (2).69a7842e.jpeg"
+  },
+  {
+    "revision": "c95b13a11857fd0e4ea15f0c983f290a",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.03 PM.c95b13a1.jpeg"
+  },
+  {
+    "revision": "a3f339b08c9b70e537a6574dc691d445",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.04 PM (1).a3f339b0.jpeg"
+  },
+  {
+    "revision": "b3f3b3f0d9bf637e8f98ad22bab4f2c3",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.04 PM (2).b3f3b3f0.jpeg"
+  },
+  {
+    "revision": "4eedb015ce0a1a6ec6da18be13a4570a",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.04 PM.4eedb015.jpeg"
+  },
+  {
+    "revision": "ddcee9806b9d1a8e222b8b0c1b3822e3",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.05 PM (1).ddcee980.jpeg"
+  },
+  {
+    "revision": "e4339f9069d983d4d95671ceda9e5131",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.05 PM (2).e4339f90.jpeg"
+  },
+  {
+    "revision": "8d6db00a236b0f52138241be8e19df83",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.05 PM.8d6db00a.jpeg"
+  },
+  {
+    "revision": "17ea46a2a1706c3f555e4f3944c1f386",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.06 PM (1).17ea46a2.jpeg"
+  },
+  {
+    "revision": "85ceb3a2eb965ac22d8a43de6983c369",
+    "url": "/static/media/WhatsApp Image 2020-07-16 at 7.12.06 PM.85ceb3a2.jpeg"
+  },
+  {
+    "revision": "20e6cab832a41e269084be8b30b5d5c7",
+    "url": "/static/media/aboutUsTopImage.20e6cab8.jpg"
+  },
+  {
+    "revision": "09dfd23aab8f56c3cce443fab62c0a3f",
+    "url": "/static/media/bg.09dfd23a.jpg"
+  },
+  {
+    "revision": "e0a2f1e15d785faf6813b9b5cb2f4de5",
+    "url": "/static/media/brain.e0a2f1e1.png"
+  },
+  {
+    "revision": "f5dbbfc20620f864f4b30adffd00ae4e",
+    "url": "/static/media/complete_logo.f5dbbfc2.png"
+  },
+  {
+    "revision": "565ab0b2683065ca6bdf19b4b262405b",
+    "url": "/static/media/short_logo.565ab0b2.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
